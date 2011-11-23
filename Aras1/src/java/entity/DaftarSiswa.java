4198f3eba313a59ffc1f01682bdcbb158ac15cbe
margo.utomo20 <margo.utomo20@gmail.com>
2011-11-23 14:39:09 +0000

    perbaikan dengan penambahan method yang belum pada Daftar Siswa

--- Aras1/src/java/entity/DaftarSiswa.java
+++ Aras1/src/java/entity/DaftarSiswa.java
@@ -8,8 +8,10 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityNotFoundException;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+import jpa.exceptions.NonexistentEntityException;
 
 /**
  *
@@ -39,6 +41,62 @@ public class DaftarSiswa {
         }
         return siswax;
     }
+    
+    public Siswa findSiswa(Integer id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Siswa.class, id);
+        } finally {
+            em.close();
+        }
+    }
+    
+    public void addSiswa(Siswa siswa) {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(siswa);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+    
+    public void updateSiswa(Siswa siswa) {
+        EntityManager em = getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.merge(siswa);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+    
+    public void deleteSiswa(Integer id) throws NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Siswa siswa;
+            try {
+                siswa = em.getReference(Siswa.class, id);
+                siswa.getId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The siswa with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(siswa);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+    
     
     
 }
