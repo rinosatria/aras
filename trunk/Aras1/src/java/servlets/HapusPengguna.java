9bd02b93f916e43cbe2c924d047e3f4a7daf385e
margo.utomo20@gmail.com <margo.utomo20@gmail.com@d44cf2ea-bb85-4036-dd4a-31664024597e>
2012-01-01 13:54:37 +0000

    Issue 19 - pembuatan servlet untuk hapus pengguna / UC 27 yang tertinggal

---
+++ trunk/Aras1/src/java/servlets/HapusPengguna.java
@@ -0,0 +1,106 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import entity.DaftarPengguna;
+import entity.Pengguna;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import jpa.exceptions.NonexistentEntityException;
+
+/**
+ *
+ * @author margoutomo
+ */
+public class HapusPengguna extends HttpServlet {
+
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, NonexistentEntityException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        HttpSession sessionedit=request.getSession();
+        
+        DaftarPengguna penggunax = new DaftarPengguna();
+        Pengguna pengguna = (Pengguna) sessionedit.getAttribute("pengguna");
+        Long id = pengguna.getId();
+        
+        penggunax.deletePengguna(id);
+        response.sendRedirect("pengguna");
+        
+        try {
+            /* TODO output your page here
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet HapusPengguna</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet HapusPengguna at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+             */
+        } finally {            
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(HapusPengguna.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(HapusPengguna.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
